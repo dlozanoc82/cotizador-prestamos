2ebcc857c4df7675ba164ba7d9845df0a8f491fe
Daniel <dlozanoc862@gmail.com>
2023-04-23 17:00:40 -0500

    :sparkles: Se añade un input para el rango del prestamo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react"
 import Header from "./components/Header"
 
 function App() {
+
+    const [amount, setAmount] = useState(10000);
+
+    const MIN = 0;
+    const MAX = 20000;
+    const STEP = 100;
+
+    function handleChange(e){
+        setAmount(+e.target.value);
+    }
+
     return (
         <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
             <Header/>
+
+            <input  
+                className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+                type="range"
+                onChange={handleChange}
+                min={MIN}
+                max={MAX}
+                step={STEP}
+                value={amount}
+            />
+
+            <p className="text-center my-10 text-5xl font-extrabold text-teal-600">{amount} <span className="text-gray-500">USD</span></p>
+
+            
+
         </div>
     )
 }
